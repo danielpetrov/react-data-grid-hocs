34daaa58ae59a26fe15aa95d89dee1a0ee0d449b
dpetrov <daniel.petrov@tick42.com>
2017-06-06 15:03:01 +0300

    updated filterable header cell and updated npm version

--- src/components/FilterableHeaderCell.js
+++ src/components/FilterableHeaderCell.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { PureComponent as Component } from 'react'
 import { string, func } from 'prop-types'
 
 export class FilterableHeaderCell extends Component {
-    constructor () {
+    constructor() {
         super()
 
         this.state = {
@@ -10,31 +10,32 @@ export class FilterableHeaderCell extends Component {
         }
     }
 
-    onChange = ({ target: { value: filterTerm } }) => {
+    onChange = (e) => {
+        const { target: { value: filterTerm } } = e
         const { columnKey, onChange } = this.props
+
+        e.stopPropagation()
 
         this.setState({ filterTerm })
         onChange({ filterTerm, column: { key: columnKey } })
     }
 
-    render () {
+    render() {
         const { filterTerm } = this.state
         const { columnKey } = this.props
 
         return (
-            <div>
-                <div className="form-group" style={{ padding: '5px 0 0 0', marginBottom: 0 }}>
-                    <input
-                        key={`header-filter-${columnKey}`}
-                        type="text"
-                        className="form-control input-md"
-                        placeholder="Search"
-                        value={filterTerm}
-                        onChange={this.onChange}
-                        onClick={e => e.stopPropagation()}
-                    />
-                </div>
-            </div>
+            <input
+                style={{ marginTop: '5px' }}
+                key={`header-filter-${columnKey}`}
+                type="text"
+                className="form-control input-md"
+                placeholder="Search"
+                value={filterTerm}
+                onChange={this.onChange}
+                onClick={e => e.stopPropagation()}
+                onKeyDown={e => e.stopPropagation()}
+            />
         )
     }
 }
